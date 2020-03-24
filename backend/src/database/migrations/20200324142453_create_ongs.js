1d4b79cacc7d40f6898e5e801316fcf91990498e
André Dourado <andre@MacBook-Pro-de-Andre.local>
2020-03-24 17:30:44 -0300

    Implementação do backend

---
+++ backend/src/database/migrations/20200324142453_create_ongs.js
@@ -0,0 +1,16 @@
+// o que é preciso ser feito
+exports.up = function(knex) {
+  return knex.schema.createTable('ongs', function (table) {
+      table.string('id').primary();
+      table.string('name').notNullable();
+      table.string('email').notNullable();
+      table.string('whatsapp').notNullable();
+      table.string('city').notNullable();
+      table.string('uf', 2).notNullable();
+  });
+};
+
+// caso ocorra algum problema 
+exports.down = function(knex) {
+  return knex.schema.dropTable('ongs');
+};
